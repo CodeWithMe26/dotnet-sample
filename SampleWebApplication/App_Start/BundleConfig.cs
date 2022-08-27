bd73280aa20b7f15ea6f7c3f7f96961770f57116
CodeWithMe26 <69313103+CodeWithMe26@users.noreply.github.com>
2022-08-27 16:49:40 +0530

    Update BundleConfig.cs

--- SampleWebApplication/App_Start/BundleConfig.cs
+++ SampleWebApplication/App_Start/BundleConfig.cs
@@ -5,7 +5,7 @@ namespace SampleWebApplication
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862 azure devops jira
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
